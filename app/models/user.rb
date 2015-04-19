b1709008e0c181fe0b6cf01b5203a8272af14b53
KillerCases <jo.killercases@gmail.com>
2015-04-19 14:20:12 +0000

    google oauth working, no image, only on dev

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,24 +5,25 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
   
   def self.from_omniauth(auth)
-  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    user.email = auth.info.email
-    user.password = Devise.friendly_token[0,20]
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
+    end
   end
-end
 
-def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
-    data = access_token.info
-    user = User.where(:email => data["email"]).first
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+      data = access_token.info
+      user = User.where(:email => data["email"]).first
 
-    # Uncomment the section below if you want users to be created if they don't exist
-    # unless user
-    #     user = User.create(name: data["name"],
-    #        email: data["email"],
-    #        password: Devise.friendly_token[0,20]
-    #     )
-    # end
-    user
-end
+  #     Uncomment the section below if you want users to be created if they don't exist
+      unless user
+          user = User.create(name: data["name"],
+             email: data["email"],
+             password: Devise.friendly_token[0,20]
+          )
+      end
+      user
+  end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150412181816) do
+ActiveRecord::Schema.define(version: 20150419135528) do
 
   create_table "bookings", force: true do |t|
     t.datetime "created_at"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 20150412181816) do
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
