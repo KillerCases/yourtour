1c21186c48a9e5151bddf4b7a10cca9996033026
KillerCases <jo.killercases@gmail.com>
2015-05-03 22:37:18 +0000

    added roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
   
+  ROLES = %w[admin guide basic]
+  
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
@@ -27,5 +29,7 @@ class User < ActiveRecord::Base
       end
       user
   end
+  
+
   
 end
