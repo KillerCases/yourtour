7cf582dfa392a349c97c92a0e45578327570901b
KillerCases <jo.killercases@gmail.com>
2015-05-11 19:59:46 +0000

    removed weird migration

--- db/migrate/20150419223137_remove_images_from_user.rb
+++
@@ -1,8 +0,0 @@
-class RemoveImagesFromUser < ActiveRecord::Migration
-  def change
-    remove_column :users, :user_image_file_name, :string
-    remove_column :users, :user_image_content_type, :string
-    remove_column :users, :user_image_file_size, :integer
-    remove_column :users, :user_image_updated_at, :datetime
-  end
-end
