9717e2ffcfab7ef23ed808cbb6f0e0620fab0050
KillerCases <jo.killercases@gmail.com>
2015-05-22 00:40:26 +0000

    quick changes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,7 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
       user ||= User.new # guest user (not logged in)
+      can :read, Tour
   
    if user.role == 'admin'
         can :manage, :all
