50c5b91c9786d963c26994100fa75e9abcb16f49
KillerCases <jo.killercases@gmail.com>
2015-05-22 00:00:35 +0000

    trying to put validation to prevent creating null booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -15,9 +15,9 @@ class BookingsController < ApplicationController
   end
   
   def calculate_total
-    total_adult = (@booking.count_adult * @booking.calendar.tour.tour_price.price_adult)
-    total_child = (@booking.count_child * @booking.calendar.tour.tour_price.price_child)
-    return total_adult + total_child  
+      total_adult = (@booking.count_adult.to_i * @booking.calendar.tour.tour_price.price_adult)
+      total_child = (@booking.count_child.to_i * @booking.calendar.tour.tour_price.price_child)
+      return total_adult + total_child 
   end
 
   def show
@@ -25,9 +25,16 @@ class BookingsController < ApplicationController
   end
 
   def new 
-    @booking = Booking.new
     @calendar = Calendar.find(params[:calendar_id]) 
-    respond_with(@booking)
+    
+    if @calendar.calendar_datetime >= Date.today
+      @booking = Booking.new
+      respond_with(@booking)
+    else
+      flash[:alert] = 'Please select a date in future'
+      redirect_to tour_path(:id => @calendar.tour.id)
+    end
+    
   end
 
   def edit
@@ -38,30 +45,30 @@ class BookingsController < ApplicationController
 #     @booking = Booking.new(booking_params)
 #     @booking.save
 #     respond_with(@booking)
-
-    @booking = Booking.new(booking_params)
+    
     @calendar = Calendar.find(params[:calendar_id]) 
+    @booking = Booking.new(booking_params)
     @booking.user_id = current_user.id
     @booking.calendar_id = @calendar.id
     @booking.total = calculate_total
     @booking.save
+      
+    
 
-    respond_to do |format|
-      if @booking.save
-        format.html { redirect_to new_charge_path(:booking_id => @booking.id), notice: 'Please confirm payment' }
-        format.json { render :show, status: :created, location: @booking }
-      else
-        format.html { render :new }
-        format.json { render json: @booking.errors, status: :unprocessable_entity }
-      end
+      respond_to do |format|
+        if @booking.save 
+          format.html { redirect_to new_charge_path(:booking_id => @booking.id), notice: 'Please confirm payment' }
+          format.json { render :show, status: :created, location: @booking }
+        else
+          format.html { render :new }
+          format.json { render json: @booking.errors, status: :unprocessable_entity }
+        end
+        
     end
-   
   end
 
   def update
     @booking.update(booking_params)
-    @booking.total = calculate_total
-    redirect_to new_charge_path(:booking_id => @booking.id), notice: 'Please confirm payment'
   end
 
   def destroy
@@ -71,7 +78,7 @@ class BookingsController < ApplicationController
     else
       @booking.destroy
         respond_to do |format|
-        format.html { redirect_to bookings_url, notice: 'Booking was successfully destroyed. Refund is ' }
+          format.html { redirect_to bookings_url, notice: 'Booking was successfully deleted.' }
         format.json { head :no_content }
       end
     end 
