d9d5baa4787ed767df9aee9e05b5b20a806c7cca
KillerCases <jo.killercases@gmail.com>
2015-06-03 01:10:38 +0000

    updating config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ Rails.application.initialize!
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'yourtour-100816.nitrousapp.com:3000',
+    :domain => 'yourtour-heroku.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
