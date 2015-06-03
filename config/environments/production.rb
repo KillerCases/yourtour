941a3b4158fe65a318e6649fe19e4beade2aac90
KillerCases <jo.killercases@gmail.com>
2015-06-03 01:29:42 +0000

    trying again after cookie clearing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,15 +92,5 @@ Rails.application.configure do
   }
   }
   
-  # Sendgrid 
-  ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'yourtour-heroku.herokuapp.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
   
 end
