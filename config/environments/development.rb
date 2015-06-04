02f5a1fa66e6ca488a9fee013de700702647614e
KillerCases <jo.killercases@gmail.com>
2015-06-04 01:49:32 +0000

    final attempt before bed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,14 +39,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :enable_starttls_auto => true
-}
+
   
   # Amazon S3 settings for Paperclip uploads
   config.paperclip_defaults = {
