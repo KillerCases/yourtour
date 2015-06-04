9d99bdbec1eba2d4141d91cee2843974680b8085
KillerCases <jo.killercases@gmail.com>
2015-06-04 01:43:34 +0000

    updating config.action_mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,8 +91,21 @@ Rails.application.configure do
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
   }
-  
+
+  #Sendgrid
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.logger = true
+  config.action_mailer.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
   
   
 end
