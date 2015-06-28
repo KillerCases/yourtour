c782996891bd374fd450df6ee292d5343495f046
KillerCases <jo.killercases@gmail.com>
2015-06-28 23:57:31 +0000

    basic email is working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,21 +35,6 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   
-  # Sendgrid
-#   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-#   config.action_mailer.delivery_method = :smtp
-#   config.action_mailer.perform_deliveries = true
-#   config.action_mailer.raise_delivery_errors = true
-#   config.action_mailer.smtp_settings = {
-#   :address        => 'smtp.sendgrid.net',
-#   :port           => '587',
-#   :authentication => :plain,
-#   :user_name      => ENV['SENDGRID_USERNAME'],
-#   :password       => ENV['SENDGRID_PASSWORD'],
-#   :enable_starttls_auto => true
-# }
-
-  
   # Amazon S3 settings for Paperclip uploads
   config.paperclip_defaults = {
   :storage => :s3,
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,22 +91,6 @@ Rails.application.configure do
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
   }
-
-#   #Sendgrid
-#   config.action_mailer.raise_delivery_errors = true
-#   config.action_mailer.delivery_method = :smtp
-#   config.action_mailer.default_url_options = { host: 'yourtour-heroku.herokuapp.com' }
-#   config.action_mailer.perform_deliveries = true
-#   config.action_mailer.logger = true
-#   config.action_mailer.smtp_settings = {
-#   :address        => 'smtp.sendgrid.net',
-#   :port           => '587',
-#   :authentication => :plain,
-#   :user_name      => ENV['SENDGRID_USERNAME'],
-#   :password       => ENV['SENDGRID_PASSWORD'],
-#   :domain         => 'heroku.com',
-#   :enable_starttls_auto => true
-# }
   
   
 end
