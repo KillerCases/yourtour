364530c7c78b47693f35a499ec28828a29bd7a28
KillerCases <jo.killercases@gmail.com>
2015-06-29 22:26:59 +0000

    reverted to user

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -3,8 +3,8 @@ class UserNotifier < ActionMailer::Base
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_booking_confirmation_email(user)
-    @booking = booking
-    mail( :to => current_user.email,
-    :subject => 'Thanks for making a booking' )
+    @user = user
+    mail( :to => user.email,
+    :subject => 'Thank you for your booking' )
   end
 end
