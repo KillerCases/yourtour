933210f42f7363db6544ce349b1022237c802f01
KillerCases <jo.killercases@gmail.com>
2015-07-06 01:20:48 +0000

    search functionality working but boxes are small

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,19 +2,22 @@ class WelcomeController < ApplicationController
   
   
   def index
-    
-    if params[:search_location].present?
-      if params[:search].present?
+    if params[:start].present?
+    time_range = (Date.strptime(params[:start], '%m/%d/%Y') - 1.day)..(Date.strptime(params[:end], '%m/%d/%Y') + 1.day)
+    end
+
+    if params[:search_location].blank?
+      if params[:start].blank?
+        @tours = Tour.search(params[:search]).order("created_at DESC")
+      else
+        @tours = Tour.search(params[:search]).order("created_at DESC").joins(:calendars).where(calendars: {calendar_datetime: time_range}) 
+      end
+    elsif
+      if params[:start].blank?
         @tours = Tour.near(params[:search_location], 50).search(params[:search]).order("created_at DESC")
       else
-        @tours = Tour.near(params[:search_location], 50)
+        @tours = Tour.near(params[:search_location], 50).search(params[:search]).order("created_at DESC").joins(:calendars).where(calendars: {calendar_datetime: time_range}) 
       end
-    elsif params[:search].present?
-      @tours = Tour.search(params[:search]).order("created_at DESC")
-    elsif params[:start].present?
-      
-      @tours = Tour.joins(:calendars)
-      
     else
       @tours = Tour.all
     end
